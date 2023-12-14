3d8d597c11dd53ebeedbd275032a0ae6f0dc983f
Allan Musyoka <musyokaallan96@gmail.com>
2023-12-14 14:36:29 +0300

    Added desktop version

--- src/Huddle.jsx
+++ src/Huddle.jsx
@@ -6,35 +6,35 @@ function Huddle() {
 
     return(
 
-        <main className='h-[350px] sm:h-[400px] border-4 border-yellow-500 py-10 sm:max-w-[90%] sm:mx-auto'>
+        <main className='h-[350px] sm:h-[600px] md:h-auto min-[820px]:h-screen min-[1023px]:h-auto py-10 sm:max-w-[90%] min-[992px]:max-w-[100%] min-[1024px]:max-w-[95%] sm:mx-auto min-[992px]:grid min-[992px]:content-center'>
 
             {/* Container */}
 
-            <div className="">
+            <div className="min-[820px]:grid min-[820px]:content-center">
 
-                <header>
+                <header className=" min-[992px]:py-10">
 
-                    <img src={Logo} alt="Logo" className="w-[170px] sm:w-[250px] cursor-pointer" />
+                    <img src={Logo} alt="Logo" className="w-[170px] sm:w-[250px] min-[992px]:w-[200px] cursor-pointer" />
 
                 </header>
 
-                <section>
+                <section className="min-[992px]:grid min-[992px]:grid-cols-2 min-[992px]:gap-10">
 
-                    <img src={HeroImage} alt="Hero" className="w-full py-10" />
+                    <img src={HeroImage} alt="Hero" className="w-full py-10 h-full" />
 
-                    <div className="border-4 border-sky-800 text-center">
+                    <div className="text-center min-[992px]:text-left min-[820px]:grid min-[820px]:content-center">
 
-                        <h1 className="capitalize text-white text-[23px] sm:text-[45px] pb-4">build the community <br /> your fans will love</h1>
+                        <h1 className="capitalize text-white text-[23px] sm:text-[45px] pb-4 min-[1280px]:pb-7 min-[992px]:text-[40px] min-[1280px]:text-[50px]">build the community <br /> your fans will love</h1>
 
-                        <p className="text-white text-[14px] sm:text-[25px]">Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussions.</p>
+                        <p className="text-white text-[14px] sm:text-[16px] min-[1280px]:text-[18px]">Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussions.</p>
 
-                        <button className="block w-[250px] sm:w-[300px] max-w-[100%] py-3 sm:py-5 mx-auto my-6 rounded-[100px] cursor-pointer bg-white sm:text-[25px]">Register</button>
+                        <button className="block w-[250px] sm:w-[300px] lg:w-[250px] max-w-[100%] py-3 sm:py-5 mx-auto my-6 min-[1280px]:my-11 rounded-[100px] cursor-pointer bg-white sm:text-[25px] min-[992px]:ml-0 min-[1024px]:text-[22px]">Register</button>
 
                     </div>
 
                 </section>
 
-                <footer className="flex justify-center items-center gap-3 py-10">
+                <footer className="flex justify-center items-center gap-3 py-10 min-[992px]:justify-end">
 
                     <i class='bx bxl-facebook'></i>    
                     <i class='bx bxl-twitter'></i>
